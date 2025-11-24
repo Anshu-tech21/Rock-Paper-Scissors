a5781cb0e9c8082fb875e446f1a83e975af77bf0
Anshu Tingre <anshu.tingre2129@gmail.com>
2025-11-24 22:12:53 +0530

    Modifing the functions in js file

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
- let ComputerMove='';
+ const score = {
+  wins:0,
+  looses: 0,
+  ties: 0
+ } 
+
+        function playGame(playerMove){
+          const ComputerMove= pickComputerMove();
+         
+         let result = '';
+         
+         if(playerMove == 'Scissors'){
+          if(ComputerMove==='Rock'){
+            result = 'Loose'
+         }else if(ComputerMove=='Paper'){
+            result='Win';
+         }else if(ComputerMove=='Scissors'){
+            result='Tie';
+         }
+
+         }else if(playerMove == 'paper'){
+      
+         if(ComputerMove==='Rock'){
+            result = 'Win'
+         }else if(ComputerMove=='Paper'){
+            result='Tie';
+         }else if(ComputerMove=='Scissors'){
+            result='Loose';
+         }
+         }else if(playerMove == 'rock'){
+  
+         if(ComputerMove==='Rock'){
+            result = 'Tie'
+         }else if(ComputerMove=='Paper'){
+            result='Loose';
+         }else if(ComputerMove=='Scissors'){
+            result='Win';
+         }
+         }
+         
+        alert(`You Picked ${playerMove} . Computer Picked ${ComputerMove} . ${result}`);
+        }
+ 
         function pickComputerMove(){
              const randomNumber = Math.random();
-        
+          let ComputerMove='';
          if(randomNumber >=0 && randomNumber < 1/3){
            ComputerMove='Rock';
          }else if(randomNumber >= 1/3 && randomNumber < 2/3){
@@ -9,4 +51,5 @@
          }else if(randomNumber >=2/3 && randomNumber < 1){
              ComputerMove='Scissors';
          }
+         return ComputerMove;
         }
